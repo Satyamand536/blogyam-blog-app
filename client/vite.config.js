1aa3e0bb2dd45108789a4966203e7a5bf7f822f9
Satyam <satyamand536@gmail.com>
2026-02-07 21:19:06 +0530

    api url changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: ['VITE_', 'NEXT_PUBLIC_'], // Support NEXT_PUBLIC_ variables
   optimizeDeps: {
     include: ['react-quill-new', 'react-quill-new/dist/quill.snow.css']
   }
